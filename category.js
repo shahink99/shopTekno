0a4f37a93d5536dca169599aecc7dab9b9cb09e7
shahink99 <150616556+shahink99@users.noreply.github.com>
2025-05-11 13:47:34 +0330

    Update category.js

--- category.js
+++ category.js
@@ -17,7 +17,7 @@ function renderCategoryProducts(productsData) {
     const card = document.createElement("div");
     card.className = "product-card";
     card.innerHTML = `
-      <img src="../assets/images/${product.image}" alt="${product.title}">
+      <img src="assets/images/${product.image}" alt="${product.title}">
       <h3>${shortten(product.title, 20)}</h3>
       <p>${shortten(product.description, 20)}</p>
       <p class="price">${product.price.toLocaleString()} تومان</p>
